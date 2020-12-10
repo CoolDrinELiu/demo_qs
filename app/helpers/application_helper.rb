4e00080493e226e92178ccd43b2dac84200226dc
DrinE Liu <drine@client.com.au>
2020-12-10 18:05:36 +0800

    added some css

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def active_menu(controllers, options = {})
+    class_name = options.fetch(:value, 'active')
+    actions = options.fetch(:actions, [])
+
+    controllers.map! { |c| File.join(options[:namespace], c) } if options[:namespace] and options[:namespace].is_a?(String)
+    return class_name if controllers.include?(params[:controller]) && actions.include?(params[:action])
+    return class_name if controllers.include?(params[:controller]) && actions.blank?
+    return nil
+  end
 end
