5ea7f82c546803b739239fc91c747ae1a0be304d
DrinE Liu <drine@client.com.au>
2020-12-12 11:34:49 +0800

    1

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,6 +3,14 @@
 //= require turbolinks
 //= require jquery3
 //= require bootstrap-sprockets
+//= require plugins/js/vendor.bundle.base
+//= require plugins/js/chart
+//= require plugins/js/off-canvas.js
+//= require plugins/js/hoverable-collapse
+//= require plugins/js/vendor.bundle.base
+//= require plugins/js/misc.js
+//= require plugins/js/dashboard
+//= require plugins/js/todolist
 
 document.addEventListener("turbolinks:load", function(){
   
