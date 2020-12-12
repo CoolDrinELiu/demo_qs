0007e4f73168c47c60092d4d838b50746ea47c49
DrinE Liu <drine@client.com.au>
2020-12-12 12:00:53 +0800

    merge

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,16 +2,19 @@
 //= require activestorage
 //= require turbolinks
 //= require jquery3
-//= require bootstrap-sprockets
 //= require plugins/js/vendor.bundle.base
-//= require plugins/js/chart
-//= require plugins/js/off-canvas.js
+
+// plugins/js/chart
+
+//= require plugins/js/off-canvas
 //= require plugins/js/hoverable-collapse
 //= require plugins/js/vendor.bundle.base
-//= require plugins/js/misc.js
-//= require plugins/js/dashboard
+//= require plugins/js/misc
+// plugins/js/dashboard
 //= require plugins/js/todolist
 
 document.addEventListener("turbolinks:load", function(){
-  
+  // $('.navbar-nav>li>a').on('click', function(){
+  //   $('.navbar-collapse').collapse('hide');
+  // });
 });
